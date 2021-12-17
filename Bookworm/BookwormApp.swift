2d4908cfb42480d94990b01890972750ac1e23ca
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2021-12-16 20:01:04 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Dayton Bobbitt on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
